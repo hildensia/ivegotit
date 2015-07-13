a115d74d3934e670633f04b603014cfd11b0441e
Johannes Kulick <kulick@hildensia.de>
2015-07-13 14:25:04 +0200

    js code for updateing entries

---
+++ static/js/entries.js
@@ -0,0 +1,35 @@
+/**
+ * Created by johannes on 7/13/15.
+ */
+function removeEntry(list_id, entry_id) {
+    $.post('/entry/rm', {
+        gi_list: list_id,
+        entry_id: entry_id
+    }).done(function(removedEntry) {
+        $('#entry' + entry_id).slideUp();
+    }).fail(function() { });
+}
+
+function addEntry(list_id, entry) {
+    $.post('/entry/add', {
+        gi_list: list_id,
+        entry: entry
+    }).done(function(addedEntry) {
+        $("#add-entry").before(addedEntry['html']);
+        $("#newentry").val('');
+        $("#entry"+addedEntry['entry_id']).hide();
+        $("#entry"+addedEntry['entry_id']).slideDown();
+    }).fail(function() { });
+}
+
+function replaceEntry(fnc, list_id, entry_id) {
+    $.post(fnc, {
+        gi_list: list_id,
+        entry_id: entry_id
+    }).done(function(gotit_Entry) {
+        $('#entry' + entry_id).fadeOut("fast", function(){
+            $(this).replaceWith(gotit_Entry['html']);
+            $('#entry' + entry_id).hide().fadeIn("fast");
+        });
+    }).fail(function() { });
+}
